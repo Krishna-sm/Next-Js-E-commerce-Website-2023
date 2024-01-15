859203e1782d6cf9aba0a25f99224835295d62e8
krishna-sm <krishna.web18@gmail.com>
2024-01-15 21:42:10 +0530

    add nodemailer types

--- lib/services/Mail.service.ts
+++ lib/services/Mail.service.ts
@@ -1,22 +1,22 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 
-const transport = nodemailer.createTransport({
-auth:{
-    user:"",
-    pass:''
-},
-port:587,
-secure:true,
-host:'smtp.gmail.com'
-});
+// const transport = nodemailer.createTransport({
+// auth:{
+//     user:"",
+//     pass:''
+// },
+// port:587,
+// secure:true,
+// host:'smtp.gmail.com'
+// });
 
 
 
-export const SendEmail = async({html,subject,to}:any)=>{
-            transport.sendMail({
-                from:'bugswear.com',
-                to:to,
-                subject:subject,
-                html:html
-            })
-}
+// export const SendEmail = async({html,subject,to}:any)=>{
+//             transport.sendMail({
+//                 from:'bugswear.com',
+//                 to:to,
+//                 subject:subject,
+//                 html:html
+//             })
+// }
